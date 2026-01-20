7449b8f52e28ae0be5bf737cd9b986ac0971faaa
sudarshanmane <sudrshanmane2110@gmail.com>
2026-01-20 20:18:16 +0530

    Refactor searchElement function to improve clarity and remove unused variables

--- BinaryCountTarget.js
+++ BinaryCountTarget.js
@@ -6,18 +6,15 @@ let input = [1, 2, 2, 2, 2, 5, 6, 8, 10, 11, 13, 13, 13, 45];
 const searchElement = (arr, el) => {
   let start = 0;
   let end = arr.length - 1;
-  let ans;
-
-  let count = 0;
+  let ans = 0;
+  let ans2 = 0;
 
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
 
-    console.log(start, mid, end);
     if (arr[mid] === el) {
       ans = mid;
       end = mid - 1;
-      count++;
     } else if (arr[mid] > el) {
       end = mid - 1;
     } else {
@@ -25,7 +22,25 @@ const searchElement = (arr, el) => {
     }
   }
 
-  console.log(ans, arr[ans], count);
+  start = 0;
+  end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === el) {
+      ans2 = mid;
+      start = mid + 1;
+    } else if (arr[mid] > el) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  
+    console.log(ans, arr[ans], ans, ans2);
+
+    return
 };
 
 searchElement(input, target);
